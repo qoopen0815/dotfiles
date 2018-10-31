e5abdd1ca1153bc2bbb7aa10e5fb85c5bd0fb0f2
calm0815 <calm0815@example.com>
2018-10-31 15:13:37 +0900

    first commit

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 10s
+killall conky
+cd "/home/kuwano/.conky/Gotham"
+conky -c "/home/kuwano/.conky/Gotham/Gotham" &
+cd "/home/kuwano/.conky/TeejeeTech"
+conky -c "/home/kuwano/.conky/TeejeeTech/Network Panel" &
+cd "/home/kuwano/.conky/TeejeeTech"
+conky -c "/home/kuwano/.conky/TeejeeTech/Process Panel" &
